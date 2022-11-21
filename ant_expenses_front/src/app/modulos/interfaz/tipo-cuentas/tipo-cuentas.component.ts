f367566ad6a2e8f685826b695d3edfd47a4a4852
german gomez <72925605+grgomezg@users.noreply.github.com>
2022-11-20 20:44:02 -0500

    Update tipo-cuentas.component.ts

--- ant_expenses_front/src/app/modulos/interfaz/tipo-cuentas/tipo-cuentas.component.ts
+++ ant_expenses_front/src/app/modulos/interfaz/tipo-cuentas/tipo-cuentas.component.ts
@@ -1,15 +1,28 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tipo-cuentas',
   templateUrl: './tipo-cuentas.component.html',
-  styleUrls: ['./tipo-cuentas.component.css']
+  styleUrls: ['./tipo-cuentas.component.css'],
 })
 export class TipoCuentasComponent implements OnInit {
+  cuentaForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.cuentaForm = this.fb.group({
+      cuentatipo: ['', [Validators.required,Validators.minLength(3)]],
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  crearCuenta(){
+    console.log(this.cuentaForm);
+
   }
 
 }
+
